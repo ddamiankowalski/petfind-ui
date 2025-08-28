6da665f69ee0468881f2b01cbfbeca045a100198
Damian <dkowalski.1997@gmail.com>
2025-08-28 22:58:51 +0200

    added third page

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,12 @@
+import TabBar from "@/components/TabBar";
 import { Tabs } from "expo-router";
 
 export default function TabsLayout() { 
   return (
-    <Tabs>
+    <Tabs tabBar={props => <TabBar {...props} />}>
       <Tabs.Screen name="index" options={{ title: 'All pets' }}></Tabs.Screen>
       <Tabs.Screen name="find-pet" options={{ title: 'Find pet' }}></Tabs.Screen>
+      <Tabs.Screen name="find-pet2" options={{ title: 'Find pet2' }}></Tabs.Screen>
     </Tabs>
   )
 }
---
+++ components/TabBar.tsx
@@ -0,0 +1,96 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import {
+  useLinkBuilder,
+  useTheme,
+} from "@react-navigation/native";
+import {
+  Text,
+  PlatformPressable,
+} from "@react-navigation/elements";
+import { BottomTabBarProps } from "@react-navigation/bottom-tabs";
+import { Feather } from '@react-native-vector-icons/feather';
+
+export default function TabBar({ state, descriptors, navigation }: BottomTabBarProps) {
+  const { colors } = useTheme();
+  const { buildHref } = useLinkBuilder();
+
+  return (
+    <View style={styles.tabbar}>
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel as React.ReactNode
+            : options.title !== undefined
+            ? options.title
+            : route.name;
+
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: "tabPress",
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name, route.params);
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: "tabLongPress",
+            target: route.key,
+          });
+        };
+
+        return (
+          <PlatformPressable
+            key={route.key}
+            android_ripple={{ color: 'transparent' }}
+            href={buildHref(route.name, route.params)}
+            accessibilityState={isFocused ? { selected: true } : {}}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarButtonTestID}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            style={styles.tabbarItem}
+          >
+            <Feather name="home" size={24} color={'#222'} />
+            <Text style={{ color: colors.text }}>
+              {label}
+            </Text>
+          </PlatformPressable>
+        );
+      })}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  tabbar: {
+    position: 'absolute',
+    bottom: 50,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    marginHorizontal: 80,
+    paddingVertical: 15,
+    borderRadius: 35,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 10 },
+    shadowRadius: 10,
+    shadowOpacity: 0.1
+  },
+  tabbarItem: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 5
+  }
+})
