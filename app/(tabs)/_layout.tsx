d3c05a0c4f8dda75d9ab45af8671a1da39bc5249
Damian <dkowalski.1997@gmail.com>
2025-08-28 23:42:42 +0200

    wip

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,17 @@
 import TabBar from "@/components/TabBar";
 import { Tabs } from "expo-router";
 
-export default function TabsLayout() { 
+export default function TabsLayout() {
   return (
-    <Tabs tabBar={props => <TabBar {...props} />}>
-      <Tabs.Screen name="index" options={{ title: 'All pets', headerShown: false, animation: 'shift' }}></Tabs.Screen>
-      <Tabs.Screen name="find-pet" options={{ title: 'Find pet', headerShown: false, animation: 'shift' }}></Tabs.Screen>
-      <Tabs.Screen name="about" options={{ title: 'About', headerShown: false, animation: 'shift' }}></Tabs.Screen>
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+      }}
+      tabBar={props => <TabBar {...props} />}
+    >
+      <Tabs.Screen name="index" options={{ title: 'All pets', animation: 'fade' }} />
+      <Tabs.Screen name="find-pet" options={{ title: 'Find pet', animation: 'fade' }} />
+      <Tabs.Screen name="about" options={{ title: 'About', animation: 'fade' }} />
     </Tabs>
   )
 }
