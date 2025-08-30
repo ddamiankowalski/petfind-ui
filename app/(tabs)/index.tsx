a3d458a8a2ded0d38a4b1edd6a0b57668ac2f68a
Damian <dkowalski.1997@gmail.com>
2025-08-30 21:42:57 +0200

    wip

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,6 +7,12 @@ export default function TabPage() {
       <MapView
         provider={PROVIDER_GOOGLE}
         style={styles.map}
+        region={{
+         latitude: 37.78825,
+         longitude: -122.4324,
+         latitudeDelta: 0.015,
+         longitudeDelta: 0.0121,
+       }}
       />
     </View>
   )
